f2df42ba71d64ebc18c391c8fc72a64f2e00f4f6
Riceball LEE <snowyu.lee@gmail.com>
2026-08-12 17:25:43 +0800

    test: import describe,it,expect for ts hint

--- src/tool-func.test.ts
+++ src/tool-func.test.ts
@@ -1,4 +1,4 @@
-import { vi } from 'vitest';
+import { describe, expect, it, vi } from 'vitest';
 import { funcGetMeta, funcWithMeta, FuncMetaSymbol, ToolFunc } from './';
 
 describe('funcWithMeta', () => {
